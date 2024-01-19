dee5c37e3a7853ccbdf45f81db4e433657f98535
Gcl010813 <17550228932@163.com>
2024-01-19 18:33:25 +0800

    ...

--- day1/babel.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    //中括号 务必注意
-    plugins: [['@babel/plugin-proposal-decorators', { legacy: true }]],
-}
--- exercises/heima-article/src/utils/request.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-/* 获取文章的网址  后续需要其他网址时可导入 */
-const request = axios.create({
-  baseURL: 'https://applet-base-api-t.itheima.net'
-})
-
-export default request
